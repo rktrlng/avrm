becf168bd90644ea6c9d9eae1f9fcad6064f204e
rktrlng <rik.teerling@gmail.com>
2015-01-10 00:35:28 +0000

    reset_gpio.sh to funcs

--- arduino_cmake/lib/boards/reset_gpio.sh
+++ arduino_cmake/lib/boards/reset_gpio.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-# tristate SPI pins
-gpio unexportall
-for i in 7 8 9 10 11; do gpio -g mode $i in; done
-for i in 7 8 9 10 11; do gpio -g mode $i tri; done
-echo "unexport done"
+reset_spi()
+{
+	gpio unexportall
+	for i in 7 8 9 10 11; do gpio -g mode $i in; done
+	for i in 7 8 9 10 11; do gpio -g mode $i tri; done
+	
+	echo "tristate spi done"
+}
 
-# reset pin high to enable AVR
-gpio -g mode 8 out
-gpio -g write 8 1
-echo "reset off"
+reset_on()
+{
+	gpio -g mode 8 out
+	gpio -g write 8 0
+	echo "reset off -> AVR in reset"
+}
+
+reset_off()
+{
+	gpio -g mode 8 out
+	gpio -g write 8 1
+	echo "reset off -> AVR running"
+}
+
+reset_spi
+reset_on
+reset_off
